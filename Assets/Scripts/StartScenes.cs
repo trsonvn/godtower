c19c6b6ebc2fe6e96ee9184cbbc31d3c3f315e9b
DESKTOP-F4PCRSI\HP <trsonvn@gmail.com>
2017-09-21 11:21:15 +0700

    no message

---
+++ Assets/Scripts/StartScenes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartScenes : MonoBehaviour {
+	public Button begin;
+	public string nextScenes;
+	// Use this for initialization
+	void Start () {
+		begin.onClick.AddListener (StartGames);
+	}
+	public void StartGames(){
+		SceneManager.LoadScene (nextScenes);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/gameControler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class gameControler : MonoBehaviour {
+	public Text hintText;
+	public InputField inputfield;
+	public Text levelText;
+
+	//normal variable
+	public string levelAnswer;
+	public string levelNumber;
+	public string levelHint;
+	public string nextScenes;
+
+	//
+	void Start(){
+		hintText.text = levelHint;
+
+	}
+	public void CheckAnswer(){
+		if(inputfield.text == levelAnswer){
+			hintText.text = "ACCESS SUCCESSFUL";
+			hintText.color = Color.green;
+			SceneManager.LoadScene (nextScenes);
+			
+		}else{
+			hintText.text = "ACCESS DENIED!!!";
+			hintText.color = Color.red;
+			inputfield.text = "";
+			inputfield.ActivateInputField ();
+		}
+	}
+	//Update is called once per frame
+	void Update(){
+		
+	}
+}
